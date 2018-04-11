5dd6c53396009c5078bebb4524c0c56575298e82
Kavish joshi <kavishjoshi@Kavishs-MacBook-Air.local>
2018-04-11 07:35:56 +0530

    //commit

--- Haptic Feedback/ViewController.swift
+++ Haptic Feedback/ViewController.swift
@@ -32,10 +32,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     @IBAction func errorBtnPressed(_ sender: Any) {
         generator.notificationOccurred(.error)
